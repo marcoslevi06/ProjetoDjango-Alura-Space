3ae01d9b1d894672ad7152e9bd4359bd42bd85d5
Marcos Levi <levi13.marcos@gmail.com>
2024-01-07 11:32:06 -0300

    templates e views

--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-from django.http import HttpResponse
-
 
 def index(request):
-    print('Request type:', request.method)
-    print(HttpResponse)
 
-    return HttpResponse('<h1>Olá, mundo!</h1>')
+    print('Request TYPE:', request.method)
+
+    return render(request, 'galeria/index.html')
