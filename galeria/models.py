1cf51c2da38c6bc665df0cc0af40423f22220e82
Marcos Levi <levi13.marcos@gmail.com>
2024-01-07 15:36:08 -0300

    comecando com persistencia de dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Fotografia(models.Model):
+
+    nome = models.CharField(max_length=200, null=False, blank=False)
+    legenda = models.CharField(max_length=200, null=True, blank=True)
+    descricao = models.TextField(null=True, blank=True)
+    foto = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.nome} - {self.legenda}'
--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from galeria.models import *
 
 def index(request):
 
     print('Request TYPE:', request.method)
 
-    return render(request, 'galeria/index.html')
+    fotografia = Fotografia.objects.all()
+    print('Fotografia:', fotografia)
+
+    # for f in fotografia:
+    #     print(f.nome, f.legenda, f.descricao, f.foto)
+
+    return render(request, 'galeria/index.html', {'fotografia': fotografia})
 
 
 def imagem(request):
