6f10eeef92d8b86a854e51384180a1d8089515f3
Marcos Levi <levi13.marcos@gmail.com>
2024-01-07 13:16:20 -0300

    conclusao da primeira etapa

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from galeria.views import *
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem)
+    path('', index, name='index'),
+    # nome da rota, função que será executada(nome da View), name='nome da rota'
+    path('imagem/', imagem, name='imagem'),
 ]
